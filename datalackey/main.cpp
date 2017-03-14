4de8b6ec252dc3b9e6240f9104dc6d8ef5a02668
ismo-karkkainen <3896372+ismo-karkkainen@users.noreply.github.com>
2017-03-14 20:47:13 +0200

    Initial Commit

---
+++ datalackey/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  datalackey
+//
+//  Created by Ismo Kärkkäinen on 14.3.17.
+//  Copyright © 2017 Ismo Kärkkäinen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
